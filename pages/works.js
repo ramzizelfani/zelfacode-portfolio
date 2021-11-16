13e7e6dd1c7fd62cdbfb4d481bd2dd0efd8d627d
Ramzi Zelfani <ramzi.zelfani@gmail.com>
2021-11-16 11:16:26 +0100

    fix(public/images): fix thumbnail images extensions

--- pages/works.js
+++ pages/works.js
@@ -3,8 +3,8 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
-import thumbSmartq from '../public/images/works/smartq_eyecatch.png'
-import thumbZelfa100 from '../public/images/works/zelfa100_eyecatch.png'
+import thumbSmartq from '../public/images/works/smartq_eyecatch'
+import thumbZelfa100 from '../public/images/works/zelfa100_eyecatch'
 
 const Works = () => (
   <Layout title="Works">
