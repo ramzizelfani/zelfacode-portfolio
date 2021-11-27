1b58b9e1515f7c1b0d70d0c6c33486257f14ef67
Ramzi Zelfani <ramzi.zelfani@gmail.com>
2021-11-27 16:39:10 +0100

    fix(*): trying to fix the vercel build problem by modifying next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack5: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false
