4626d52dbc921b46536307ae39445e8bbd1b52dd
Ramzi Zelfani <ramzi.zelfani@gmail.com>
2021-11-27 16:13:37 +0100

    fix(*): add webpack to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+    }
+    return config
+  }
 }
